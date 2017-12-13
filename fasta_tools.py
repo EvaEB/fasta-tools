087acd7125c43492dd909df05c346b472df5821b
bonse <eva.bons@env.ethz.ch>
2017-12-13 16:28:02 +0100

    highlighter plot properly sorted

--- fasta_tools.py
+++ fasta_tools.py
@@ -2,17 +2,24 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read_fasta(filename):
+
+def read_fasta(filename,consensus=0):
     '''
     reads in a fasta file as dictionary with sequence identifiers as keys and
     sequences as values
     '''
     seqs = {}
+    counter = 0
     with open(fasta_file) as f:
         for line in f.readlines():
             if '>' in line:
-                current = line.strip()[1:]
-                seqs[current] = ''
+                if counter == consensus:
+                    seqs['consensus'] = ''
+                    current = 'consensus'
+                else:
+                    current = line.strip()[1:]
+                    seqs[current] = ''
+                counter+=1
             else:
                 seqs[current]+=line.strip()
 
@@ -25,13 +32,30 @@ def skyline(seqs=None,filename=None):
     colors = {'A': '#bf6c60', 'T': '#99cc33', 'G': '#2a3326', 'C': '#36b8d9', '-': '#792080'}
     if seqs is None:
         seqs = read_fasta(filename)
-    seq_len = len(seqs.values()[0])
-    first = seqs.keys()[0]
-    for i,idx in enumerate(seqs):
-        plt.plot([0,seq_len],[i,i],color='grey')
-        changes = np.where(np.array(list(seqs[idx])) != np.array(list(seqs[first])))
-        for change in changes[0]:
-            plt.scatter(change,i,color=colors[seqs[idx][change]])
+    to_plot = seqs.keys()
+    seq_len = len(seqs[seqs.keys()[0]])
+    current = 'consensus'
+    pos = 0
+    mins=None
+    while len(to_plot) > 0:
+        plt.plot([0,seq_len],[pos,pos],color='grey')
+        changes = [p for p,a, b in zip(range(seq_len),seqs['consensus'], seqs[current]) if a != b]
+        to_plot.remove(current)
+        for change in changes:
+            plt.scatter(change,pos,color=colors[seqs[current][change]])
+        mins = None
+        for i in to_plot:
+            changes = sum(1 for a, b in zip(seqs[current], seqs[i]) if a != b)
+            if changes == 0:
+                next_current = i
+                mins = 0
+                break
+            elif mins is None or changes < mins:
+                mins = changes
+                next_current = i
+        current = next_current
+        pos -= 1
+
     plt.axis('off')
     plt.show()
 
